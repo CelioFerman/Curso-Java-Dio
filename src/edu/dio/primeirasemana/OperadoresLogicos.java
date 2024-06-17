57f2f42fcbadad7b8ae23c82a91c14426950d3da
Célio Fermano <celiolucio94@hotmail.com>
2024-06-17 11:58:58 -0300

    Criando Operadores Lógicos
    Criando a classe de operadores lógicos.

---
+++ src/edu/dio/primeirasemana/OperadoresLogicos.java
@@ -0,0 +1,11 @@
+package edu.dio.primeirasemana;
+public class OperadoresLogicos {
+    
+    static void main (String[] args) {
+
+        String nomeCompletoCurso = "Linguage" + "Java";
+        System.out.println(nomeCompletoCurso);
+
+    }
+}
+ 
