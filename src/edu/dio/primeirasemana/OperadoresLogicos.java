8d03ff7022e4c3d9d29ff8762375da44026354e3
Célio Fermano <celiolucio94@hotmail.com>
2024-06-17 14:43:47 -0300

    New Updates
    novos atualizações em operadores lógicos

--- src/edu/dio/primeirasemana/OperadoresLogicos.java
+++ src/edu/dio/primeirasemana/OperadoresLogicos.java
@@ -1,7 +1,8 @@
 package edu.dio.primeirasemana;
+
 public class OperadoresLogicos {
     
-    static void main (String[] args) {
+    public  static void main (String[] args) {
 
         String concatenacao = "?";
 
