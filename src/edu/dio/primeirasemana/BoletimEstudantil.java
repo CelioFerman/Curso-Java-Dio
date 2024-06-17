af445d52574bc2ebf191168c704db68c9535abc9
Célio Fermano <celiolucio94@hotmail.com>
2024-06-17 11:00:16 -0300

    Firt week
    Var's and classes

---
+++ src/edu/dio/primeirasemana/BoletimEstudantil.java
@@ -0,0 +1,29 @@
+package edu.dio.primeirasemana;
+
+public class BoletimEstudantil {
+
+    public static void main(String[] args) {
+
+        // Com Indentação
+        int mediaFinal = 6;
+
+        if (mediaFinal < 6)
+            System.out.println("Reprovado");
+        else if (mediaFinal == 6)
+            System.out.println("Prova Minerva");
+        else
+            System.out.println("Aprovado");
+
+        // Sem Indentação!
+
+        // int mediaFinal = 7;
+        // if(mediaFinal<6)
+        // System.out.println("Reprovado");
+        // else if(mediaFinal == 6)
+        // System.out.println("Prova Minerva");
+        // else
+        // System.out.println("Aprovado");
+
+    }
+
+}
---
+++ src/edu/dio/primeirasemana/MinhaClasse.java
@@ -0,0 +1,16 @@
+package edu.dio.primeirasemana;
+public class MinhaClasse {
+    public static void main (String[] args){
+        
+        String primeiroNome = "Célio";
+        String segundonome = "Ferman";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundonome);
+        System.out.println(nomeCompleto);
+
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "Resultado do metodo " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
