0e52ae145fb0f00f03f598f5467d68de9ea44fb8
Celio Fermano <celiolucio94@hotmail.com>
2024-06-18 12:02:10 -0300

    Utilizando Scanner
    Mudando a forma para entrada de dados com o Scanner

--- src/edu/dio/primeirasemana/sobreMin.java
+++ src/edu/dio/primeirasemana/sobreMin.java
@@ -1,15 +1,23 @@
 package edu.dio.primeirasemana;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class sobreMin {
     public static void main(String[] args) {
-        String nome = args [0];
-        String sobrenome = args [1];
-        int idade = Integer.valueOf(args[2]);
-        int altura = Integer.valueOf(args[3]);
 
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("Olá, me chamo" + nome + "" + sobrenome);
-        System.out.println("Tenho " + idade +" anos");
-        System.out.println("Minha altura é " + altura + "cm");
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome: ");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite sua altura: ");
+        int altura = scanner.nextInt();
     }
 }
