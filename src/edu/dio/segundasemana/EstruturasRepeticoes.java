b31db268e7fb6262ba8f7216e93ffec98f7beb53
Celio Fermano <celiolucio94@hotmail.com>
2024-06-18 17:11:33 -0300

    Criando a estrutura If-Else
    criando um sistema que utiliza da condição if-else; para sacar um dinheiro.

---
+++ src/edu/dio/segundasemana/EstruturasRepeticoes.java
@@ -0,0 +1,24 @@
+package edu.dio.segundasemana;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class EstruturasRepeticoes {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        double saldo = 25.0;
+
+        System.out.println("Digite o valor solicitado: ");
+
+        double valorSolicitado = scanner.nextDouble();
+
+        if (valorSolicitado < saldo) {
+            saldo = saldo - valorSolicitado;
+        } else {
+            System.out.println("Saldo não compativel ao valor solicitado");
+            System.out.println(saldo);
+        }
+    }
+}
