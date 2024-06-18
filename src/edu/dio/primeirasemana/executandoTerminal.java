2a63cbca82f14bcdedf0906a7211ff1ed9f069c7
Celio Fermano <celiolucio94@hotmail.com>
2024-06-18 11:31:57 -0300

    Criando classe executandoTerminal
    Classe criada para executar o programa fora da area de desenvolvimento.

---
+++ src/edu/dio/primeirasemana/executandoTerminal.java
@@ -0,0 +1,9 @@
+package edu.dio.primeirasemana;
+
+public class executandoTerminal {
+    public static void main(String[] args) {
+        System.out.println("Executando pelo terminal.");
+        
+    }
+
+}
