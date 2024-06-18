6ec2eaaa704e2573899a2758fcd9e9040da405f4
Celio Fermano <celiolucio94@hotmail.com>
2024-06-17 21:45:51 -0300

    Criando calculadora basica
    Criação da calculadora para ver os arquivos de documentação do Java;

---
+++ src/edu/dio/primeirasemana/calculadora.java
@@ -0,0 +1,17 @@
+package edu.dio.primeirasemana;
+
+public class calculadora {
+    public static void main(String[] args) {
+        
+    
+
+    
+    int primeiroNumero = 10;
+   int segundoNumero = 7;
+
+   int resolucao = primeiroNumero + segundoNumero;
+   System.out.println(resolucao);
+   
+   
+}
+}
