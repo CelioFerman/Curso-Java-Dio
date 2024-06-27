4762afcf5ca97fc958172f337913097d3e997e1c
Celio Fermano <celiolucio94@hotmail.com>
2024-06-27 10:43:56 -0300

    Construtores
    Criando objetos atravez de construtores

---
+++ src/edu/dio/terceiraSemana/SistemaCadastro.java
@@ -0,0 +1,11 @@
+package edu.dio.terceiraSemana;
+
+public class SistemaCadastro {
+    public static void main(String[] args) {
+        Pessoa marcos = new Pessoa("123", "Marcos");
+
+        marcos.setEndereco("Rua das mulas");
+
+        System.out.println(marcos.getNome() + " - " + marcos.getCpf());
+    }
+}
